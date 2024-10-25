b29480e52a67e7063b7272b0c3ed6830d7f97a19
Symon Barreto <contatosymon@gmail.com>
2024-10-25 12:21:56 -0400

    baseUrl config

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,9 +14,7 @@ describe("Orange HRM Tests", () => {
     dashboard: "/web/index.php/dashboard/index",
   };
   it("Successful login", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.loginButton).click();
@@ -24,9 +22,7 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.dashboardGrid);
   });
   it("Error login", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userFail.username);
     cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
