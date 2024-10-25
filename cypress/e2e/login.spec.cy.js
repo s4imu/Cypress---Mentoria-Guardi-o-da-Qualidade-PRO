a5606528d5032dd8eb8880fc8b3bd54c60df56b8
Symon Barreto <contatosymon@gmail.com>
2024-10-25 09:42:27 -0400

    Spec login successful

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,12 @@
+describe("Orange HRM Tests", () => {
+  it("Successful login", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get('input[name="username"]').type("Admin");
+    cy.get('input[name="password"]').type("admin123");
+    cy.get("button.orangehrm-login-button").click();
+    cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+    cy.get(".oxd-topbar-header-breadcrumb-module").contains("Dashboard");
+  });
+});
